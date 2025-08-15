d751074f0d1ae0b0a61e492b6fecfa3914caffe0
nihal866 <nihaldwivedi111@gmail.com>
2025-08-15 14:11:00 +0530

    add component scannning

--- spring-core/src/main/java/com/nihal/springcoredemo/SpringcoredemoApplication.java
+++ spring-core/src/main/java/com/nihal/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,16 @@ package com.nihal.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+if we want to include packages, out of this directory
+then we explicitly scan packages like this
+
+<--
+
+@SpringBootApplication(scanBasePackages = { "com.nihal.springcoredemo", "com.nihal.util" })
+
+-->
+*/
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
