4f7ca917792328c79860c8cf3fd7ac5aff8ae25b
nihal866 <nihaldwivedi111@gmail.com>
2025-08-18 19:16:49 +0530

    restful apis development

---
+++ spring_boot_rest_crud_apis/src/main/java/com/nihal/restDemo/controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.nihal.restDemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nihal.restDemo.entity.Student;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private List<Student> studentsList;
+
+    // using @PostContruct for loading students list.... only once
+    @PostConstruct
+    public void createStudentsList() {
+        studentsList = new ArrayList<>();
+
+        studentsList.add(new Student("Nihal", "Dwivedi"));
+        studentsList.add(new Student("Shubham", "Kumar"));
+        studentsList.add(new Student("Chandan", "Kumar"));
+        studentsList.add(new Student("Pratik", "Gupta"));
+    }
+
+    @GetMapping("/list")
+    public List<Student> getStudentLists() {
+        return studentsList;
+    }
+
+    @GetMapping("/list/{studentId}")
+    public Student getStudentDetails(@PathVariable int studentId) throws Exception {
+        if (studentId >= studentsList.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return studentsList.get(studentId);
+    }
+}
