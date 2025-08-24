ef8a400273bdd02070e3dd4c9db6c97d7adb648d
nihal866 <nihaldwivedi111@gmail.com>
2025-08-24 21:37:56 +0530

    add patch(update) api

--- spring-boot-rest-crud-employee-project/src/main/java/com/nihal/demo/controller/DemoRestController.java
+++ spring-boot-rest-crud-employee-project/src/main/java/com/nihal/demo/controller/DemoRestController.java
@@ -1,10 +1,12 @@
 package com.nihal.demo.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.nihal.demo.entity.Employee;
 import com.nihal.demo.service.EmployeeService;
 
@@ -20,10 +24,12 @@ import com.nihal.demo.service.EmployeeService;
 public class DemoRestController {
 
     private EmployeeService employeeService;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public DemoRestController(EmployeeService employeeService) {
+    public DemoRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
@@ -58,5 +64,32 @@ public class DemoRestController {
     @DeleteMapping("/employees/{employeeId}")
     public void deleteEmployee(@PathVariable int employeeId) {
         employeeService.deleteById(employeeId);
+    }
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee partialUpdateEmployee(@PathVariable int employeeId,
+            @RequestBody Map<String, Object> patchVariable) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee with id: " + employeeId + " not found.");
+        }
+
+        if (patchVariable.containsKey("id")) {
+            throw new RuntimeException("Can not pass id with the body.");
+        }
+
+        Employee employee = mergePatchChanges(tempEmployee, patchVariable);
+        return employeeService.save(employee);
+    }
+
+    private Employee mergePatchChanges(Employee tempEmployee, Map<String, Object> patchVariable) {
+        ObjectNode employeeData = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        ObjectNode patchEmployeeData = objectMapper.convertValue(patchVariable, ObjectNode.class);
+
+        employeeData.setAll(patchEmployeeData);
+        return objectMapper.convertValue(employeeData, Employee.class);
     }
 }
