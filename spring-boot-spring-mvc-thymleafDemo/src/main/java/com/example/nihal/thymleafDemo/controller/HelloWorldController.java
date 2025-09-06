3655b35d8c4c947bfe1362f4d240f9aa78367251
nihal866 <nihaldwivedi111@gmail.com>
2025-09-06 14:27:14 +0530

    revised changes for thymleaf demo

--- spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/controller/HelloWorldController.java
+++ spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/controller/HelloWorldController.java
@@ -3,7 +3,10 @@ package com.example.nihal.thymleafDemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -13,10 +16,19 @@ public class HelloWorldController {
         return "hellowworld-form";
     }
 
-    @GetMapping("/process")
+    @PostMapping("/process")
     public String processForm(Model model, @RequestParam(name = "studentName") String studName) {
         System.out.println("Entered name: " + studName);
         model.addAttribute("studName", studName);
         return "process";
+    }
+
+    @PostMapping("/processSecond")
+    public String processV2Form(HttpServletRequest request, Model model) {
+        String studName = request.getParameter("studentName");
+        studName = studName.toUpperCase();
+        String message = "Hello " + studName;
+        model.addAttribute("message", message);
+        return "process-beta";
     }
 }
