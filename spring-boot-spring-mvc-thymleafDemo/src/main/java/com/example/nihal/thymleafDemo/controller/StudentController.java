9e15fbc5ef59568b821b8347d31242d8d736b64e
nihal866 <nihaldwivedi111@gmail.com>
2025-09-08 06:42:21 +0530

    add spring mvc form data binding code

---
+++ spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.nihal.thymleafDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.nihal.thymleafDemo.model.Student;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/showForm")
+    public String showStudentForm(Model model) {
+        Student newStudent = new Student();
+        model.addAttribute("student", newStudent);
+        return "student-show-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processStudentForm(@ModelAttribute("student") Student student) {
+        System.out.println(student.toString());
+        return "student-process";
+    }
+}
