c7d34df4846abfd0598dd4711cc704ef5c517e56
nihal866 <nihaldwivedi111@gmail.com>
2025-09-08 07:07:55 +0530

    add more spring mvc form data binding code

--- spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/controller/StudentController.java
+++ spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.nihal.thymleafDemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +16,22 @@ import com.example.nihal.thymleafDemo.model.Student;
 @RequestMapping("/student")
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${hobbies}")
+    private List<String> hobbies;
+
     @GetMapping("/showForm")
     public String showStudentForm(Model model) {
         Student newStudent = new Student();
         model.addAttribute("student", newStudent);
+        model.addAttribute("countries", countries);
+        model.addAttribute("languages", languages);
+        model.addAttribute("hobbies", hobbies);
         return "student-show-form";
     }
 
--- spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/model/Student.java
+++ spring-boot-spring-mvc-thymleafDemo/src/main/java/com/example/nihal/thymleafDemo/model/Student.java
@@ -1,11 +1,17 @@
 package com.example.nihal.thymleafDemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
 
     private String lastName;
 
     private String country;
+
+    private String language;
+
+    private List<String> hobbies;
 
     public Student() {
     }
@@ -34,8 +40,25 @@ public class Student {
         this.country = country;
     }
 
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
     @Override
     public String toString() {
-        return "Student [firstName=" + firstName + ", lastName=" + lastName + ", country=" + country + "]";
+        return "Student [firstName=" + firstName + ", lastName=" + lastName + ", country=" + country + ", language="
+                + language + ", hobbies=" + hobbies + "]";
     }
 }
